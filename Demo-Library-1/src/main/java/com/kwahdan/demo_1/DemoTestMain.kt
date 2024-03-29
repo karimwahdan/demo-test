fe535341341adbae3fbd4afea83d08c6015b4b66
Karim Wahdan <dr.karimwahdan@gmail.com>
2024-03-30 00:56:32 +0200

    init

---
+++ Demo-Library-1/src/main/java/com/kwahdan/demo_1/DemoTestMain.kt
@@ -0,0 +1,8 @@
+package com.kwahdan.demo_1
+
+class DemoTestMain {
+
+    fun sayHello(text:String): String {
+        return text
+    }
+}
---
+++ app/src/main/java/com/karimwahdan/demoapplication/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.karimwahdan.demoapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.karimwahdan.demoapplication.ui.theme.DemoApplicationTheme
+import com.kwahdan.demo_1.DemoTestMain
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DemoApplicationTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting(DemoTestMain().sayHello("Hello"))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    DemoApplicationTheme {
+        Greeting("Android")
+    }
+}
